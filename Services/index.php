036ec1d768a11ecaea70db51635ee2f8ea9b8759
MD. SAKIBUL ISLAM <mdsakibulislam736@gmail.com>
2023-12-19 23:32:02 +0600

    1

---
+++ Services/index.php
@@ -0,0 +1,113 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../AdminPannel/style.css">
+    <title>
+        Doctors
+    </title>
+</head>
+<body>
+
+        
+    
+    <section>
+      <div class="row">
+        <h2 class="section-heading">Our Services</h2>
+      </div>
+      <div class="row">
+        
+        <div class="column">
+          <div class="card">
+            <!-- <div class="icon-wrapper">
+              <i class="fas fa-hammer"></i>
+            </div>-->
+            <a href="http://localhost/MedicareHub/">
+            <h3> Doctors </h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
+              consequatur necessitatibus eaque.
+            </p>
+          </a>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+          <!--  <div class="icon-wrapper">
+              <i class="fas fa-brush"></i>
+            </div>-->
+            <a href="Ambulance.html">
+            <h3>Ambulances</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
+              consequatur necessitatibus eaque.
+            </p>
+          </a>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+        <!--    <div class="icon-wrapper">
+              <i class="fas fa-wrench"></i>
+            </div>-->
+            <a href="Medicine.html">
+            <h3>Medicines</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
+              consequatur necessitatibus eaque.
+            </p>
+          </a>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+        <!--   <div class="icon-wrapper">
+              <i class="fas fa-truck-pickup"></i>
+            </div>--> 
+            <a href="self.html">
+            <h3>Self Checkup</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
+              consequatur necessitatibus eaque.
+            </p>
+          </a>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+          <!--  <div class="icon-wrapper">
+              <i class="fas fa-broom"></i>
+            </div>-->
+            <h3>Hospitals</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
+              consequatur necessitatibus eaque.
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+          <!--   <div class="icon-wrapper">
+              <i class="fas fa-plug"></i>
+            </div>-->
+            <h3>Others</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam
+              consequatur necessitatibus eaque.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+<script type="text/javascript" src="../AdminPannel/script.js" >
+
+</script>
+</body>
+</html>
